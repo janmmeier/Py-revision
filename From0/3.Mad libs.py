11cefc804d3efaf5a0d9100138ba673ea97b2f33
janmmeier <Jan-Markus.Meier@tptlive.ee>
2021-07-22 18:17:37 +0300

    fixed names, added mad libs and lists

---
+++ From0/3.Mad libs.py
@@ -0,0 +1,14 @@
+water = input("Please enter a water source: ")
+animal = input("Please enter an animal: ")
+
+print("An old silent " + water + "…")
+print("A " + animal + " jumps into the pond, splash!")
+print("Silence again.")
+
+"""
+Furuike ya
+kawazu tobikomu
+mizu no oto
+
+-- Basho
+"""
---
+++ From0/4.Lists and tuples.py
@@ -0,0 +1,21 @@
+friends = ["Kevin", "Karen", "John", "Liis", "Jim"]
+#Items in list start from 0 = Kevin, 1 = Karen, 2 = John and so on.
+print(friends)
+print(friends[3]) #Grabs 4th item on the list
+print(friends[1:]) #Grabs items from the second one
+print(friends[0:3]) #Grabs items from 0 to 3
+
+lucky_numbers = [4, 8, 15, 24, 42, 56, 68, 99]
+friends.extend(lucky_numbers) #adds lucky numbers to the friends list
+#.append() - adds something to the end of the list
+#.insert(1, "Kelly") - adds an item to the list at the needed position
+#.remove("jim") - removes items
+#.clear() - cleares the list
+#.pop() - removes a random item from the list
+print(friends.index("Kevin")) #checks if item is in the list and where is it located
+#.sort() - sorts the list in alphabetical order
+#.reverse()
+#friends2 = friends.copy() - copies the list
+
+coordinates = (4, 5)
+
